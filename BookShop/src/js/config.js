d8f509397d1e573732f011e800fdcd541686dff9
Isabell <isabell.orekhova@gmail.com>
2018-01-27 17:14:38 +0200

    fix architecture

--- BookShop/src/js/config.js
+++ BookShop/src/js/config.js
@@ -1,20 +1,12 @@
-// const GLOBAL_CONFIG = {
-//     ORIGIN: "http://localhost:9090",
-//     // ENV_HREF: "/",
-//     ENV_HREF: "/BookShop/",
-//     // PATH: '../../assets/img/',
-//     PATH: '/BookShop/public/assets/img/',
-
-// };
-
 const GLOBAL_CONFIG = {
     ORIGIN: "http://localhost:9090",
     // ENV_HREF: "/",
     ENV_HREF: "/BookShop/",
     PATH: '../../assets/img/',
-    // PATH: '/public/assets/img/',
+    // PATH: '/BookShop/public/assets/img/',
 
 };
+
 export default GLOBAL_CONFIG;
 export const ENV_HREF = GLOBAL_CONFIG.ENV_HREF;
 export const PATH = GLOBAL_CONFIG.PATH;
