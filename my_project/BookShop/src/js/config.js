2a90be3220a38314674433aa439dd1ca1747f818
Isabell <isabell.orekhova@gmail.com>
2018-01-27 17:01:20 +0200

    change BookShop

--- my_project/BookShop/src/js/config.js
+++ my_project/BookShop/src/js/config.js
@@ -1,12 +1,20 @@
+// const GLOBAL_CONFIG = {
+//     ORIGIN: "http://localhost:9090",
+//     // ENV_HREF: "/",
+//     ENV_HREF: "/BookShop/",
+//     // PATH: '../../assets/img/',
+//     PATH: '/BookShop/public/assets/img/',
+
+// };
+
 const GLOBAL_CONFIG = {
     ORIGIN: "http://localhost:9090",
     // ENV_HREF: "/",
     ENV_HREF: "/BookShop/",
-    // PATH: '../../assets/img/',
-    PATH: '/BookShop/public/assets/img/',
+    PATH: '../../assets/img/',
+    // PATH: '/public/assets/img/',
 
 };
-
 export default GLOBAL_CONFIG;
 export const ENV_HREF = GLOBAL_CONFIG.ENV_HREF;
 export const PATH = GLOBAL_CONFIG.PATH;
